4686b9e3de75533ca9888ae62c9ab4b650d18bec
Antonio Zugaldia <antonio@zugaldia.net>
2012-12-03 23:46:54 -0500

    fixed favicon and bootstrap based templates

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
 import webapp2
+import jinja2
+import os
+
+jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        template_values = {}
+        template = jinja_environment.get_template('index.html')
+        self.response.out.write(template.render(template_values))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
